b5cf992f3f225def3cecdaa3db2012865074c5ed
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-07-06 17:30:36 +0000

    Update: AUTOSAR basic software ECU abstraction module for J1939 stack - transport layer

--- LATEST/J1939Tp.cpp
+++ LATEST/J1939Tp.cpp
@@ -7,9 +7,7 @@
 /* #INCLUDES                                                                  */
 /******************************************************************************/
 #include "Module.hpp"
-#include "CfgJ1939Tp.hpp"
-#include "J1939Tp_core.hpp"
-#include "infJ1939Tp_Exp.hpp"
+#include "J1939Tp.hpp"
 #include "infJ1939Tp_Imp.hpp"
 
 /******************************************************************************/
@@ -32,27 +30,6 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
-class module_J1939Tp:
-      INTERFACES_EXPORTED_J1939TP
-      public abstract_module
-   ,  public class_J1939Tp_Functionality
-{
-   private:
-/******************************************************************************/
-/* OBJECTS                                                                    */
-/******************************************************************************/
-            infPduRClient_Lo        infPduRClient_J1939Tp;
-
-   public:
-      FUNC(void, J1939TP_CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, J1939TP_CONFIG_DATA, J1939TP_APPL_CONST) lptrCfgModule
-      );
-      FUNC(void, J1939TP_CODE) DeInitFunction (void);
-      FUNC(void, J1939TP_CODE) MainFunction   (void);
-      J1939TP_CORE_FUNCTIONALITIES
-};
-
-extern VAR(module_J1939Tp, J1939TP_VAR) J1939Tp;
 
 /******************************************************************************/
 /* CONSTS                                                                     */

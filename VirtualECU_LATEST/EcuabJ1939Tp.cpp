77f53a2dee7f4531a18a3af17021dbae8722b0e9
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:04:39 +0200

    Update: AUTOSAR basic software ECU abstraction module for J1939 stack - transport layer

--- VirtualECU_LATEST/EcuabJ1939Tp.cpp
+++ VirtualECU_LATEST/EcuabJ1939Tp.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* File   : EcuabJ1939Tp.cpp                                                       */
+/* File   : EcuabJ1939Tp.cpp                                                  */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -13,8 +13,8 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define ECUABJ1939TP_AR_RELEASE_VERSION_MAJOR                                       4
-#define ECUABJ1939TP_AR_RELEASE_VERSION_MINOR                                       3
+#define ECUABJ1939TP_AR_RELEASE_VERSION_MAJOR                                  4
+#define ECUABJ1939TP_AR_RELEASE_VERSION_MINOR                                  3
 
 /******************************************************************************/
 /* MACROS                                                                     */

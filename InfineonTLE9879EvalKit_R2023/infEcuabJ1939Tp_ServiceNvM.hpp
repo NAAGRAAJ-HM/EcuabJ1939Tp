bfd3606bcd7e37df4e9787a7ed1f8c381f7477a3
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:51:42 +0100

    Update: AUTOSAR basic software ECU abstraction module for J1939 stack - transport layer

---
+++ InfineonTLE9879EvalKit_R2023/infEcuabJ1939Tp_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infEcuabJ1939Tp_ServiceNvM.hpp                                    */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTECUABJ1939TP_AR_RELEASE_VERSION_MAJOR                             4
+#define CONSTECUABJ1939TP_AR_RELEASE_VERSION_MINOR                             3
+
+#define Const_DefaultInit_ConstEcuabJ1939Tp                                                                                \
+   ,  {CONSTECUABJ1939TP_AR_RELEASE_VERSION_MAJOR, CONSTECUABJ1939TP_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
